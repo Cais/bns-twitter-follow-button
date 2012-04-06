64494e80b127b52b52c6774725efafa029569fa2
Edward Caissie <edward.caissie@gmail.com>
2012-04-06 11:13:04 -0400

    First commit

---
+++ bns-tfbutton.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: BNS Twitter Follow Button
+Plugin URI: http://buynowshop.com/plugins/bns-tfbutton
+Description: Based on the (JavaScript) Twitter Follow Button (https://twitter.com/about/resources/followbutton) featuring all of the functionality offered including language support.
+Version: 0.2
+Author: Edward Caissie
+Author URI: http://edwardcaissie.com/
+*/
+
+/* Add function to the widgets_init hook. */
+add_action( 'widgets_init', 'load_bns_tfbutton_widget' );
+  
+/* Function that registers our widget. */
+function load_bns_tfbutton_widget() {
+	register_widget( 'BNS_TFButton_Widget' );
+}
+
+class BNS_TFButton_Widget extends WP_Widget {
+  
+	function BNS_TFButton_Widget() {
+      /* Widget settings. */
+  		$widget_ops = array( 'classname' => 'bns-tfbutton', 'description' => __( 'Twitter Follow Button' ) );
+  		/* Widget control settings. */
+  		$control_ops = array( 'width' => 200, 'height' => 200, 'id_base' => 'bns-tfbutton' );
+  		/* Create the widget. */
+  		$this->WP_Widget( 'bns-tfbutton', 'BNS Twitter Follow Button', $widget_ops, $control_ops );
+  	}
+	
+	function widget( $args, $instance ) {
+      extract( $args );
+      /* User-selected settings. */
+      /* TO-DO: Finish all available settings */
+      $title        = apply_filters('widget_title', $instance['title'] );
+      $twitter_name = $instance['twitter_name'];
+      $show_count   = $instance['show_count']; /* Followers count display */
+      $button       = $instance['button']; // Button color
+      $text_color   = $instance['text_color']; // Text color
+      $link_color   = $instance['link_color']; // Link color
+      $lang         = $instance['lang']; // Language - default: English
+      $width        = $instance['width']; // Width
+      $align        = $instance['align']; // Alignment
+      
+      /* Before widget (defined by themes). */
+      echo $before_widget;
+
+      /* Title of widget (before and after defined by themes). */
+      if ( $title )
+        echo $before_title . $title . $after_title;
+
+      /* Display stuff based on widget settings. */
+      ?>
+        <a href="http://twitter.com/<?php echo $twitter_name; ?>" class="twitter-follow-button" data-show-count=<?php !$show_count ? printf( '"false"' ) : printf( '"true"' ); ?> data-button=<?php !$button ? printf( '"blue"' ) : printf( '"grey"' ); ?> data-text-color="<?php echo $text_color; ?>" data-link-color="<?php echo $link_color; ?>" data-width=<?php printf( $width ); ?> data-align=<?php !$align ? printf( '"left"' ) : printf( '"right"' ); ?> data-lang="<?php printf( $lang ); ?>" >Follow @<?php echo $twitter_name; ?></a>
+        <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+      <?php
+            
+      /* After widget (defined by themes). */
+      echo $after_widget;
+  }
+  
+	function update( $new_instance, $old_instance ) {
+      $instance = $old_instance;
+      
+      /* Strip tags (if needed) and update the widget settings. */
+      $instance['title']        = strip_tags( $new_instance['title'] );
+      $instance['twitter_name'] = strip_tags( $new_instance['twitter_name'] );
+      $instance['show_count']   = $new_instance['show_count'];
+      $instance['button']       = $new_instance['button'];
+      $instance['text_color']   = $new_instance['text_color'];
+      $instance['link_color']   = $new_instance['link_color'];
+      $instance['lang']         = $new_instance['lang'];
+      $instance['width']        = $new_instance['width'];
+      $instance['align']        = $new_instance['align'];
+
+      return $instance;
+  }
+  
+  function form( $instance ) {
+      /* Set default widget settings. */
+      $defaults = array(
+          'title'         => __( 'Twitter Follow Button' ),
+          'twitter_name'  => '',
+          'show_count'    => false,
+          'button'        => false, // Blue
+          'text_color'    => '186487', // Default blue pallette. The default Grey color would be white (#ffffff); leave empty.
+          'link_color'    => '',
+          'lang'          => '', // Default - English
+          'width'         => '300px',
+          'align'         => ''
+      );
+      $instance = wp_parse_args( (array) $instance, $defaults );
+		?>
+    
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:'); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
+  	</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'twitter_name' ); ?>"><?php _e('Twitter Name:'); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'twitter_name' ); ?>" name="<?php echo $this->get_field_name( 'twitter_name' ); ?>" value="<?php echo $instance['twitter_name']; ?>" />
+  	</p>
+
+  	<p>
+			<input class="checkbox" type="checkbox" <?php checked( (bool) $instance['button'], true ); ?> id="<?php echo $this->get_field_id( 'button' ); ?>" name="<?php echo $this->get_field_name( 'button' ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'button' ); ?>"><?php _e('Grey button? (default: Blue)'); ?></label>
+		</p>
+
+    <p>
+  		<label for="<?php echo $this->get_field_id( 'link_color' ); ?>"><?php _e('Link Color: #'); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'link_color' ); ?>" name="<?php echo $this->get_field_name( 'link_color' ); ?>" value="<?php echo $instance['link_color']; ?>" />
+    </p>
+
+  	<p>
+			<input class="checkbox" type="checkbox" <?php checked( (bool) $instance['show_count'], true ); ?> id="<?php echo $this->get_field_id( 'show_count' ); ?>" name="<?php echo $this->get_field_name( 'show_count' ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'show_count' ); ?>"><?php _e('Show Follower Count?'); ?></label>
+		</p>
+
+    <p>
+  		<label for="<?php echo $this->get_field_id( 'text_color' ); ?>"><?php _e('Text Color: #'); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'text_color' ); ?>" name="<?php echo $this->get_field_name( 'text_color' ); ?>" value="<?php echo $instance['text_color']; ?>" />
+    </p>
+	
+  	<p>
+			<input class="checkbox" type="checkbox" <?php checked( (bool) $instance['align'], true ); ?> id="<?php echo $this->get_field_id( 'align' ); ?>" name="<?php echo $this->get_field_name( 'align' ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'align' ); ?>"><?php _e('Align right? (default: left)'); ?></label>
+		</p>
+
+    <p>
+  		<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e('Width (in pixels of percentage):'); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" value="<?php echo $instance['width']; ?>" />
+  		<label>NB: Must be set in pixels to right align.</label>
+    </p>
+    
+ 		<p>
+			<label for="<?php echo $this->get_field_id( 'lang' ); ?>"><?php _e('Language (as the two letter ISO-639-1 language code.):'); ?></label>
+			<select id="<?php echo $this->get_field_id( 'lang' ); ?>" name="<?php echo $this->get_field_name( 'lang' ); ?>" class="widefat">
+				<option <?php selected( 'en', $instance['lang'], true ); ?>>en</option>
+				<option <?php selected( 'fr', $instance['lang'], true ); ?>>fr</option>
+				<option <?php selected( 'de', $instance['lang'], true ); ?>>de</option>
+				<option <?php selected( 'it', $instance['lang'], true ); ?>>it</option>
+				<option <?php selected( 'es', $instance['lang'], true ); ?>>es</option>
+				<option <?php selected( 'ko', $instance['lang'], true ); ?>>ko</option>
+				<option <?php selected( 'ja', $instance['lang'], true ); ?>>ja</option>
+			</select>
+		</p>
+
+		<?php
+	}
+}
+/* Add shortcode from post: */ 
+function bns_tfbutton_shortcode ($atts) {
+  ob_start(); /* Start capture */
+  /* Using the_widget() to make a plugin template tag */
+  ?>
+  <div class="bns-tfbutton-shortcode">
+  <?php
+  the_widget(
+    'BNS_TFButton_Widget',
+    $instance = shortcode_atts( array(
+          'title'         => __(''),
+          'twitter_name'  => '', // No @ symbol needed
+          'show_count'    => false,
+          'button'        => false, // Blue
+          'text_color'    => '186487', // No # symbol needed
+          'link_color'    => '',
+          'lang'          => '', // default: English
+          'width'         => '300px',
+          'align'         => '', // Left aligned
+    ), $atts ),
+    $args = array (
+    'before_widget'   => '',
+    'before_title'    => '',
+    'after_title'     => '',
+    'after_widget'    => ''
+    )
+  );
+  ?>
+  </div><!-- .bns-ftbutton-shortcode -->
+  <?php
+  $bns_tfbutton_output = ob_get_contents(); /* Captured output */
+  ob_end_clean(); /* Stop capture */
+
+  return $bns_tfbutton_output;
+}
+add_shortcode( 'bns_tfbutton', 'bns_tfbutton_shortcode' );
+/* Shortcode end */
+?>
+<?php /* Last revised June 2, 2011 version 0.2 */ ?>
---
+++ readme.txt
@@ -0,0 +1,104 @@
+=== BNS Twitter Follow Button ===
+Contributors: cais
+Donate link: http://buynowshop.com
+Tags: multi-widget, user-options, twitter, multiple-accounts, language-support
+Requires at least: 2.8
+Tested up to: 3.2
+Stable tag: 0.2
+
+Displays a Twitter Follow Button; and, includes shortcode functionality.
+
+== Description ==
+
+A widget to allow you to set the parameters of the Twitter Follow Button found here: https://twitter.com/about/resources/followbutton. This widget also creates a shortcode that can be used in posts and pages. Also to note, each instance of the shortcode or widget can use a different Twitter name so you can have multiple Twitter accounts listed on your website. Includes support of languages for the Follow Button using the two letter ISO-639-1 language code for English (en), French (fr), German (de), Italian (it), Spanish (es), Korean (ko) and Japanese (ja).
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `bns-tfbutton.php` to the `/wp-content/plugins/` directory
+2. Activate through the 'Plugins' menu.
+3. Place the BNS Twitter Follow Button widget appropriately in the Appearance | Widgets section of the dashboard.
+4. Set options to personal preferences.
+
+-- or -
+
+1. Go to 'Plugins' menu under your Dashboard
+2. Click on the 'Add New' link
+3. Search for bns-twitter-follow-button
+4. Install.
+5. Activate through the 'Plugins' menu.
+6. Place the BNS Twitter Follow Button widget appropriately in the Appearance | Widgets section of the dashboard.
+7. Set options to personal preferences.
+
+Reading this article for further assistance: http://wpfirstaid.com/2009/12/plugin-installation/
+
+----
+= Shortcode: bns_tfbutton =
+Parameters are very similar to the plugin ...
+
+*   'title'         => __('')
+*   'twitter_name'  => '', // No @ symbol needed
+*   'show_count'    => false
+*   'button'        => false, // Blue
+*   'text_color'    => '186487', // No # symbol needed
+*   'link_color'    => ''
+*   'width'         => '300px'
+*   'align'         => '', // Left aligned
+*   'lang'          => '' // default English
+
+NB: Use the shortcode at your own risk!
+
+== Frequently Asked Questions ==
+
+= Where can I get support for this widget? =
+
+Please note, support may be available on the WordPress Support forums; but, it may be faster to visit http://buynowshop.com/plugins/bns-ftbutton/ and leave a comment with the issue you are experiencing.
+
+= Can I use this in more than one widget area? =
+
+Yes, this plugin has been made for multi-widget compatibility. Each instance of the widget will display, if wanted, differently than every other instance of the widget.
+
+= How can I style the plugin output? =
+
+Yes, more to follow ... or read this page: http://dev.twitter.com/pages/follow_button
+
+== Screenshots ==
+1. The options panel.
+
+== Other Notes ==
+* Copyright 2011  Edward Caissie  (email : edward.caissie@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2,
+  as published by the Free Software Foundation.
+
+  You may NOT assume that you can use any other version of the GPL.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  The license for this software can also likely be found here:
+  http://www.gnu.org/licenses/gpl-2.0.html
+
+== Upgrade Notice ==
+Please stay current with your WordPress installation, your active theme, and your plugins.
+
+== Changelog ==
+= 0.2 =
+* added support of languages for the Follow Button using the two letter ISO-639-1 language code
+* languages supported: English (en); French (fr); German (de); Italian (it); Spanish (es); Korean (ko); and, Japanese (ja).
+* updated readme (this file) with corections to shortcode usage / parameters
+
+= 0.1.1 =
+* update to readme.txt file (typos and corrections)
+
+= 0.1 =
+* Initial Release - working beta version
+* released June 1, 2011
